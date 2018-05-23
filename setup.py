e1df79e924cff751a79c38d71d9ae1b892ac84cf
Maria192 <maria.myslina@gmail.com>
2018-05-23 19:14:51 +0300

    setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'search_kwic',
+  packages = ['search_kwic'],
+  version = '0.1',
+  description = 'Tool for KWIC representation of paralleltext. Find a word in parallel text corresponding to query in original text.',
+  author = 'Maria Terekhina',
+  author_email = 'maria.myslina@gmail.com',
+  url = 'https://github.com/maria-terekhina/search_kwic',
+  download_url = 'https://github.com/maria-terekhina/search_kwic/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['kwic', 'parallel', 'corpora', 'alignment'], 
+  classifiers = [],
+)
